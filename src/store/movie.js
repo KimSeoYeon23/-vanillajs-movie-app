0a536a52ec99c294c682b1de8aab749be378e33c
SeoYeon <sollucidus0221@naver.com>
2023-02-20 20:51:00 +0900

    예외처리 메세지 출력

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,7 +8,8 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
-  loading: false
+  loading: false,
+  message: 'Search for the movie title!'
 });
 
 console.log(store.state);
@@ -19,14 +20,24 @@ export const searchMovies = async page => {
   store.state.page = page;
   if(page === 1) {
     store.state.movies = [];
+    store.state.message = '';
   }
   // 영화 리스트 api 연결
-  const res = await fetch(`${process.env.OMDb_API_KEY}s=${store.state.searchText}&page=${page}`);
-  const { Search, totalResults } = await res.json();
-  store.state.movies = [
-    ...store.state.movies,
-    ...Search
-  ]
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  store.state.loading = false;
+  try {
+    const res = await fetch(`${process.env.OMDb_API_KEY}s=${store.state.searchText}&page=${page}`);
+    const { Search, totalResults, Response, Error } = await res.json();
+    if(Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+    }
+  } catch (error) {
+    console.log('searchMovies error: ', error);
+  } finally {
+    store.state.loading = false;
+  }
 }
