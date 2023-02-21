cdbabb3d7e3841b0d9fe5b9ca30237941a93a73b
SeoYeon <sollucidus0221@naver.com>
2023-02-21 18:46:09 +0900

    skeleton ui

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -3,6 +3,15 @@ import movieStore, { getMovieDetails } from "../store/movie";
 
 export default class Movie extends Component {
   async render() {
+    this.el.classList.add('container', 'the-movie')
+    this.el.innerHTML = /* html */ `
+      <div class='poster skeleton'></div>
+      <div class='specs'>
+        <div class='title skeleton'></div>
+        <div class='labels skeleton'></div>
+        <div class='plot skeleton'></div>
+      </div>
+    `
     await getMovieDetails(history.state.id);
 
     const { movie } = movieStore.state;
@@ -10,7 +19,6 @@ export default class Movie extends Component {
 
     console.log(movie);
     
-    this.el.classList.add('container', 'the-movie')
     this.el.innerHTML = /* html */ `
       <div style="background-image: url(${bigPoster})" class='poster'></div>
       <div class='specs'>
