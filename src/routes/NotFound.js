287f0088a72db49030377997ba060843741ad912
SeoYeon <sollucidus0221@naver.com>
2023-02-22 16:02:55 +0900

    404 Page Not Found

---
+++ src/routes/NotFound.js
@@ -0,0 +1,18 @@
+import { Component } from "../core/heropy";
+
+export default class NotFound extends Component {
+  render() {
+    this.el.classList.add('container', 'not-found');
+    this.el.innerHTML = /* html */ `
+      <h1>
+        Sorry..<br/>
+        Page Not Found.
+      </h1>
+      <button>← Back</button>
+    `
+    const btnEl = this.el.querySelector('button');
+    btnEl.addEventListener('click', () => {
+      history.back();
+    })
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { createRouter } from "../core/heropy";
-import Home from './Home'
-import Movie from './Movie'
+import Home from './Home';
+import Movie from './Movie';
 import About from './About';
+import NotFount from './NotFound';
 
 export default createRouter([
   { path: '#/', component: Home },
   { path: '#/movie', component: Movie },
-  { path: '#/about', component: About }
+  { path: '#/about', component: About },
+  { path: '.*', component: NotFount }
 ]);
