4614bcb3144d6b6b1894460221a761adf64ed1bf
SeoYeon <sollucidus0221@naver.com>
2023-03-16 20:12:27 +0900

    about.js 타이핑

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,10 +1,19 @@
-import { Store } from './../core/heropy';
+import { Store } from '../core/heropy';
 
-export default new Store({
+interface State {
+  photo: string;
+  name: string;
+  email: string;
+  blog: string;
+  github: string;
+  repository: string;
+}
+
+export default new Store<State>({
   photo: 'https://i.ibb.co/p3pYK3m/bondee-profile.png',
   name: 'KimSeoYeon',
   email: 'tjdus3431@daum.net',
   blog: 'https://sy-developer.notion.site/Dev-77134d22850b420eaa856affa95c0fad',
   github: 'https://github.com/KimSeoYeon23',
   repository: 'https://github.com/KimSeoYeon23/fc-movie-app'
-})
+});
