05fa125f1925f5426bd0e43b432a97633a8350ff
SeoYeon <sollucidus0221@naver.com>
2023-03-21 14:56:47 +0900

    edit api

--- api/movie.js
+++ api/movie.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
-dotenv.config();
+const { API_KEY } = process.env;
 
 export default async function handler(request, response) {
   const { title, page, id } = JSON.parse(request.body);
   const url =  id 
-        ? `https://www.omdbapi.com/?apikey=${process.env.OMDb_API_KEY}&i=${id}&plot=full` 
-        : `https://www.omdbapi.com/?apikey=${process.env.OMDb_API_KEY}&s=${title}&page=${page}`;
+        ? `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}&plot=full` 
+        : `https://www.omdbapi.com/?apikey=${API_KEY}&s=${title}&page=${page}`;
   const res = await fetch(url);
   const json = await res.json();
   response.status(200).json(json)
